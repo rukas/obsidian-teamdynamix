7d683ceb74eaee3aa4ff2ca01ffc514d97d2bf13
Lucas Schill <lukeschill96@gmail.com>
2023-12-31 03:16:30 -0500

    Make the file read/write process more efficient and prevent issues with 'Major Incident' vs 'Incident'

--- src/updateFileFromServer.ts
+++ src/updateFileFromServer.ts
@@ -7,27 +7,38 @@ export async function updateFileFromServer(settings: TeamDynamixSettings, app: A
 
   let fileContents = await app.vault.read(file)
 
-  let changed = false;
+  // group the keywords by itemType
+  // we can't use Object.groupBy since we're on ES6
+  let itemTypesGroup = settings.keywordToItemType.reduce((acc, curr) => {
+    
+    if (!acc[curr.itemType]) acc[curr.itemType] = []; //If this type wasn't previously stored
+    acc[curr.itemType].push(curr);
+    return acc;
+  }, {});
 
-  for (const keywordToItemType of settings.keywordToItemType) {
-    if (keywordToItemType.keyword.length > 1 && keywordToItemType.itemType.length > 1 && fileContents.includes(keywordToItemType.keyword)) {
+  // build a regex string to see if there are any changes at all to make to the file
+  // this will be checked first to make sure we're not trying to edit the file for no reason
+  let globalMatchString = `(?<!\\[)(?:${settings.keywordToItemType.map(a => a.keyword).join('|')})(\\d+)(?!.*\\])`;
 
-      console.log("TeamDynamix: Updating work item id with link. If this happened automatically and you did not intend for this " +
-        "to happen, you should either disable automatic replacement of your keyword with todos (via the settings), or" +
-        " exclude this file from auto replace (via the settings).")
+  if (fileContents.match(globalMatchString)) {
 
-      const itemPath = (settings.typeToPath.find(i => i.itemType == keywordToItemType.itemType)).path;
+    for (const [key, value] of Object.entries(itemTypesGroup)) {
 
-      const formattedLink = `[${keywordToItemType.keyword}$1](${settings.teamdynamixBaseUrl}${itemPath}$1)`;
+      // build a regex string for just the keywords of the current item type
+      let localMatchString = `(?<!\\[)(${itemTypesGroup[key].map(a => a.keyword).join('|')})(\\d+)(?!.*\\])`;
 
-      const regex = new RegExp(`(?<!\\[)${keywordToItemType.keyword}(\\d+)`, `gm`);
+      // console.log(localMatchString);
 
-      fileContents = fileContents.replace(regex, formattedLink);
-      changed = true;
-    }
+      // if there are matches for the current item type perform a replacement on the file
+      if (fileContents.match(localMatchString)) {
 
-    if (changed == true) {
-      return app.vault.process(file, (data) => fileContents);
+        const itemPath = (settings.typeToPath.find(i => i.itemType == key)).path;
+        const formattedLink = `[$1$2](${settings.teamdynamixBaseUrl}${itemPath}$2)`;
+        const regex = new RegExp(localMatchString, `mig`);
+        fileContents = fileContents.replace(regex, formattedLink);
+      }
     }
   }
+  // save the file after all of the replacements have been completed
+  return app.vault.process(file, (data) => fileContents);
 }
